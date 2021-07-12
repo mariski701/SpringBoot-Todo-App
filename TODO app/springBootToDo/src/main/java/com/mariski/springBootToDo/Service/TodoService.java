7d368a37948a4bf5adad0f1100dd20e2fd0a66fe
mariski701 <mariusz.skiba1995@gmail.com>
2021-07-13 01:35:55 +0200

    Added Service
    Added service based on CRUD

--- TODO app/springBootToDo/src/main/java/com/mariski/springBootToDo/Service/TodoService.java
+++ TODO app/springBootToDo/src/main/java/com/mariski/springBootToDo/Service/TodoService.java
@@ -2,18 +2,51 @@ package com.mariski.springBootToDo.Service;
 
 import com.mariski.springBootToDo.Model.Todo;
 import com.mariski.springBootToDo.Repository.TodoRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TodoService {
 
     @Autowired
-    private TodoRepository todoService;
+    private TodoRepository todoRepository;
 
-    public static final Logger log = LoggerFactory.getLogger(TodoService.class);
+    public Todo addTodo(Todo todo) {
+        return todoRepository.save(todo);
+    }
 
+    public List<Todo> addTodos(List<Todo> todos) {
+        return todoRepository.saveAll(todos);
+    }
 
+    public Todo getTodo(int id) {
+        return todoRepository.findById(id).orElse(null);
+    }
+
+    public List<Todo> getTodos() {
+        return todoRepository.findAll();
+    }
+
+    public String updateTodo(int id, boolean done) {
+        Todo todoToUpdate = todoRepository.findById(id).orElse(null);
+        if (!todoToUpdate.equals(null)) {
+            todoToUpdate.setDone(done);
+            todoRepository.save(todoToUpdate);
+            return "State of Todo is updated";
+        }
+        else
+            return "Failed to update";
+    }
+
+    public String deleteTodo(int id) {
+        todoRepository.deleteById(id);
+        return "Todo of id " + id + " was successfully deleted";
+    }
+
+    public String deleteAllTodos() {
+        todoRepository.deleteAll();
+        return "Todos were removed";
+    }
 }
